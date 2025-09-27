c00b1ccc6f10facd241951474c67c81aaaa4e44d
maheswarankasi <maheskasi007@gmail.com>
2025-09-27 03:33:36 -0400

    mongoDB enabled

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:PNhuZHNrFWp1KtckhSfYqwLe@172.21.187.38:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
